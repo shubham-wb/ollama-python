7ac13a4fcf3046618e9edbac600e0656966cb37d
tensaidevx <shubham.gupta.npd@gmail.com>
2025-01-26 16:32:25 +0530

    added ai chat stream

--- start-2.py
+++ start-2.py
@@ -14,3 +14,20 @@ res = ollama.chat(
 )
 
 print(res["message"]["content"])
+
+
+# Chat example streaming === 
+
+res  = ollama.chat(
+    model="llama3.2",
+    messages=[
+        {
+            "role":"user",
+            "content":"why is the ocean so salty?"
+        }
+    ],
+    stream=True,
+)
+
+for chunk in res: 
+    print(chunk["message"]["content"],end="",flush=True)
